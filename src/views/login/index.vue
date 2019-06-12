df40bc626c2516269b0e6c86f3949b02c1ebd4dd
hswyxj <hesw@yyft.com>
2019-06-12 10:56:17 +0800

    Update index.vue

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,81 +1,72 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
-      <div class="title-container">
-        <h3 class="title">
-          后台管理系统
-        </h3>
-      </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="用户账号"
-          name="username"
-          type="text"
-          auto-complete="on"
-          clearable
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="用户密码"
-          name="password"
-          auto-complete="on"
-          clearable
-          show-password
-          @keyup.enter.native="handleLogin"
-        />
-        <!-- <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span> -->
-      </el-form-item>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">
-        登 录
-      </el-button>
-
-      <div style="position:relative">
-        <div class="tips">
-          <span>超级管理账号 : admin</span>
-          <span>密码 : admin_password</span>
+    <div class="login-main">
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+        <div class="title-container">
+          <h3 class="title">
+            后台管理系统
+          </h3>
         </div>
-        <div class="tips">
-          <span style="margin-right:18px;">
-            普通员工账号 : editor
+
+        <el-form-item prop="username">
+          <span class="svg-container">
+            <svg-icon icon-class="user" />
           </span>
-          <span>密码 : editor_password</span>
+          <el-input
+            ref="username"
+            v-model="loginForm.username"
+            placeholder="用户账号"
+            name="username"
+            type="text"
+            auto-complete="on"
+            clearable
+          />
+        </el-form-item>
+
+        <el-tooltip v-model="capsTooltip" content="大写锁定已开启" placement="right" manual>
+          <el-form-item prop="password">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input
+              :key="passwordType"
+              ref="password"
+              v-model="loginForm.password"
+              :type="passwordType"
+              placeholder="用户密码"
+              name="password"
+              auto-complete="on"
+              clearable
+              show-password
+              @keyup.native="checkCapslock"
+              @blur="capsTooltip = false"
+              @keyup.enter.native="handleLogin"
+            />
+          </el-form-item>
+        </el-tooltip>
+
+        <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">
+          登 录
+        </el-button>
+
+        <div style="position:relative">
+          <div class="tips">
+            <span>超级管理账号 : admin</span>
+            <span>密码 : 88888888</span>
+          </div>
         </div>
-      </div>
-    </el-form>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+// import { validUsername } from '@/utils/validate'
+
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('请输入正确的用户名'))
-      } else {
-        callback()
-      }
-    }
+    // 自定义验证
     const validatePassword = (rule, value, callback) => {
       if (value.length < 8) {
         callback(new Error('密码不能少于8位'))
@@ -86,13 +77,15 @@ export default {
     return {
       loginForm: {
         username: 'admin',
-        password: 'admin_password'
+        password: '88888888'
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+        username: [{ required: true, trigger: 'blur', message: '请输入正确的用户名' },
+          { min: 4, message: '用户账号长度 4 个字符以上', trigger: 'blur' }],
         password: [{ required: true, trigger: 'blur', validator: validatePassword }]
       },
       passwordType: 'password',
+      capsTooltip: false,
       loading: false,
       showDialog: false,
       redirect: undefined
@@ -120,20 +113,23 @@ export default {
     // window.removeEventListener('storage', this.afterQRScan)
   },
   methods: {
-    // showPwd() {
-    //   if (this.passwordType === 'password') {
-    //     this.passwordType = ''
-    //   } else {
-    //     this.passwordType = 'password'
-    //   }
-    //   this.$nextTick(() => {
-    //     this.$refs.password.focus()
-    //   })
-    // },
-    handleLogin() {
+    checkCapslock({ shiftKey, key } = {}) {
+      if (key && key.length === 1) {
+        if (shiftKey && (key >= 'a' && key <= 'z') || !shiftKey && (key >= 'A' && key <= 'Z')) {
+          this.capsTooltip = true
+        } else {
+          this.capsTooltip = false
+        }
+      }
+      if (key === 'CapsLock' && this.capsTooltip === true) {
+        this.capsTooltip = false
+      }
+    },
+    handleLogin(val) {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
           this.loading = true
+          // store.dispatch 可以处理被触发的 action 的处理函数返回的 Promise，并且 store.dispatch 仍旧返回 Promise
           this.$store.dispatch('user/login', this.loginForm)
             .then(() => {
               this.$router.push({ path: this.redirect || '/' })
@@ -152,47 +148,38 @@ export default {
 }
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
+<style lang="scss" >
+  /* 修复input 背景不协调 和光标变色 */
+  /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 $bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
-
+// $light_gray:#fff;
+// $cursor: #fff;
+$light_gray:#787982;
+$cursor:#787982;
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
     color: $cursor;
+    }
   }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
+  /* reset element-ui css */
+  .login-container {
+    .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+      input {
+      // background: transparent;
       border: 0px;
       -webkit-appearance: none;
-      border-radius: 0px;
+      border-radius: 5px;
       padding: 12px 5px 12px 15px;
       color: $light_gray;
       height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
       }
     }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    .el-form-item {
+    border: 1px solid rgb(214, 214, 216);
+    // background: rgba(0, 0, 0, 0.1);
     border-radius: 5px;
     color: #454545;
   }
@@ -203,34 +190,39 @@ $cursor: #fff;
 $bg:#2d3a4b;
 $dark_gray:#889aa4;
 $light_gray:#eee;
-
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  // background-color: url(../../assets/login.png);
+  background-image: url(../../assets/login.png);
+  background-size: cover;
   overflow: hidden;
-
+  .login-main{
+    border-radius: 16px;
+    padding: 15% 50px 25px 50px;
+  }
   .login-form {
     position: relative;
-    width: 520px;
+    left:25%;
+    width: 400px;
     max-width: 100%;
-    padding: 160px 35px 0;
+    padding: 35px 35px 0;
     margin: 0 auto;
     overflow: hidden;
+    border-radius: 5px;
+    box-sizing: border-box;
+    background-color: #fff;
   }
-
   .tips {
     font-size: 14px;
     color: #fff;
     margin-bottom: 10px;
-
     span {
       &:first-of-type {
         margin-right: 16px;
       }
     }
   }
-
   .svg-container {
     padding: 6px 5px 6px 15px;
     color: $dark_gray;
@@ -238,19 +230,24 @@ $light_gray:#eee;
     width: 30px;
     display: inline-block;
   }
-
   .title-container {
     position: relative;
-
     .title {
       font-size: 26px;
-      color: $light_gray;
+      color: $dark_gray;
       margin: 0px auto 40px auto;
       text-align: center;
       font-weight: bold;
     }
+    .set-language {
+      color: #fff;
+      position: absolute;
+      top: 3px;
+      font-size:18px;
+      right: 0px;
+      cursor: pointer;
+    }
   }
-
   .show-pwd {
     position: absolute;
     right: 10px;
@@ -260,17 +257,10 @@ $light_gray:#eee;
     cursor: pointer;
     user-select: none;
   }
-
   .thirdparty-button {
     position: absolute;
     right: 0;
     bottom: 6px;
-  }
-
-  @media only screen and (max-width: 470px) {
-    .thirdparty-button {
-      display: none;
-    }
   }
 }
 </style>
